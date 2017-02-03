fd183814449289de90dd3be2188916347146fee2
= <=>
2017-02-03 04:34:13 +0000

    Serviço base, a ser extendido por todos os outros

---
+++ src/app/base.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+export const BASE_API_URL = 'http://api-hck.hotmart.com/';
+
+@Injectable()
+export class BaseService {
+}
