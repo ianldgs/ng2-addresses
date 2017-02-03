9611c3c1d48073eccd1df7f42029e7d320f6fed3
Ian Luca <ian.naph@gmail.com>
2017-02-03 02:38:04 -0200

    Adicionando loading tosco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import { Address } from './address';
 import { AddressService } from './address.service';
+import { UserService } from './user.service';
 
 @Component({
-  providers: [AddressService],
+  providers: [AddressService, UserService],
   selector: 'my-app',
   template: `
     <h1>Hello {{name}}</h1>
     <h2>Endereços</h2>
-    <ul>
+    <span *ngIf="!addresses">Carregando...</span>
+    <ul *ngIf="addresses">
       <li *ngFor="let address of addresses">
         <address [address]="address"></address>
       </li>
@@ -20,7 +22,7 @@ export class AppComponent implements OnInit {
   name = 'Angular'; 
   addresses: Address[];
 
-  constructor(private addressService: AddressService) { }
+  constructor(private addressService: AddressService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.getAddresses();
