dc5098c78e5a9e03593c48d96478d8230ea06c8c
Ian Luca <ian.naph@gmail.com>
2017-02-03 12:15:48 -0200

    Importando módulo de forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { AddressComponent } from './address.component';
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule ],
+  imports:      [ BrowserModule, FormsModule, HttpModule ],
   declarations: [ AppComponent, AddressComponent ],
   bootstrap:    [ AppComponent ]
 })
