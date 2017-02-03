fedc8c593f4f0ada292983711e55b26ff48a5111
= <=>
2017-02-03 04:34:51 +0000

    Serviço de Usuário, para fazer login

---
+++ src/app/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import { BASE_API_URL, BaseService } from './base.service';
+
+@Injectable()
+export class UserService extends BaseService {
+  constructor(private http: Http) {
+    super();
+  }
+
+  isLogged(): boolean {
+    return !!localStorage.getItem('access_token');
+  }
+
+  login(email: string, password: string): Promise {
+    const headers = new Headers();
+    //headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', 'Basic ZTZiZGVjY2ItNzM1OC00OTk3LWIzYzAtODk2NDBhZjEyZGRlOmQ5OWNmMTU0LTFjZGYtNDRiMi04MDJmLWU1YzhiYmU5NjY5OA==');
+
+    return this.http
+      .post(
+        `${BASE_API_URL}/security/oauth/token?grant_type=password&username=${email}&password=${password}`,
+        { },
+        { headers }
+      )
+      .toPromise()
+      .then(res => res.json())
+      .then(res => {
+        localStorage.setItem('access_token', res.access_token);
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+  }
+}
