92c16c664854d718b46664d14872ca34e519743d
Ian Luca <ian.naph@gmail.com>
2017-02-03 12:39:51 -0200

    Indentação 2 espaços

--- src/app/login-form.component.ts
+++ src/app/login-form.component.ts
@@ -3,43 +3,43 @@ import { Component } from '@angular/core';
 import { UserService } from './user.service';
 
 @Component({
-    providers: [UserService],
-    selector: 'login-form',
-    template: `
-        <form name="login" (ngSubmit)="tryToLogin()">
-            <input 
-                placeholder="email" 
-                name="email"
-                type="email"
-                required
-                [(ngModel)]="email"
-            >
-            <input 
-                placeholder="senha" 
-                name="password"
-                type="password"
-                required
-                [(ngModel)]="password"
-            >
-            <button type="submit">Entrar</button>
-        </form>
+  providers: [UserService],
+  selector: 'login-form',
+  template: `
+    <form name="login" (ngSubmit)="tryToLogin()">
+      <input 
+        placeholder="email" 
+        name="email"
+        type="email"
+        required
+        [(ngModel)]="email"
+      >
+      <input 
+        placeholder="senha" 
+        name="password"
+        type="password"
+        required
+        [(ngModel)]="password"
+      >
+      <button type="submit">Entrar</button>
+    </form>
     `
     //templateUrl: './name.component.html',
     //styleUrls: ['./name.component.css']
 })
 export class LoginFormComponent {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 
-    constructor(private userService: UserService) { }
+  constructor(private userService: UserService) { }
 
-    tryToLogin() {
-        this.userService
-            .login(this.email, this.password)
-            .then(() => {
-                alert('Logou');
-            }, () => {
-                alert('Deu ruim');
-            });
-    }
+  tryToLogin() {
+    this.userService
+      .login(this.email, this.password)
+      .then(() => {
+          alert('Logou');
+      }, () => {
+          alert('Deu ruim');
+      });
+  }
 }
