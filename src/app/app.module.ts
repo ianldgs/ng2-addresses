2486f92451783270ba02994baedbe9b600dc273d
Ian Luca <ian.naph@gmail.com>
2017-02-03 12:36:40 -0200

    Componente formulário de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { LoginFormComponent } from './login-form.component';
 import { AddressComponent } from './address.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule ],
-  declarations: [ AppComponent, AddressComponent ],
+  declarations: [ AppComponent, LoginFormComponent, AddressComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/login-form.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+import { UserService } from './user.service';
+
+@Component({
+    providers: [UserService],
+    selector: 'login-form',
+    template: `
+        <form name="login" (ngSubmit)="tryToLogin()">
+            <input 
+                placeholder="email" 
+                name="email"
+                type="email"
+                required
+                [(ngModel)]="email"
+            >
+            <input 
+                placeholder="senha" 
+                name="password"
+                type="password"
+                required
+                [(ngModel)]="password"
+            >
+            <button type="submit">Entrar</button>
+        </form>
+    `
+    //templateUrl: './name.component.html',
+    //styleUrls: ['./name.component.css']
+})
+export class LoginFormComponent {
+    email: string;
+    password: string;
+
+    constructor(private userService: UserService) { }
+
+    tryToLogin() {
+        this.userService
+            .login(this.email, this.password)
+            .then(() => {
+                alert('Logou');
+            }, () => {
+                alert('Deu ruim');
+            });
+    }
+}
