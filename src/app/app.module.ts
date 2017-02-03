3dc17c32deafcb221ee22b0f352bab5c4052083f
= <=>
2017-02-03 04:35:17 +0000

    Importando modulo Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { AddressComponent } from './address.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule ],
   declarations: [ AppComponent, AddressComponent ],
   bootstrap:    [ AppComponent ]
 })
