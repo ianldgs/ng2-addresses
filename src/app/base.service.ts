c4d6cb2f5edd48586aa3039ef13f0a409ad3cfea
Ian Luca <ian.naph@gmail.com>
2017-02-03 02:41:23 -0200

    Adicionando link da documentação da API

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -3,6 +3,9 @@ import { Http } from '@angular/http';
 
 export const BASE_API_URL = 'http://api-hck.hotmart.com/';
 
+/**
+ * @link http://api-hck.hotmart.com/hack-swagger
+ */
 @Injectable()
 export class BaseService {
 }
