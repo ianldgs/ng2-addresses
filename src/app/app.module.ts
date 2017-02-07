528977e0117bbfaac56cd8b2f39c4922e95e7949
Ian Luca <ian.naph@gmail.com>
2017-02-06 23:44:31 -0200

    importando módulo de material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
 
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
@@ -19,6 +20,7 @@ import { AddressService } from './address.service';
     BrowserModule, 
     FormsModule, 
     HttpModule,
+    MaterialModule.forRoot(),
     RouterModule.forRoot([
       {
         path: '',
